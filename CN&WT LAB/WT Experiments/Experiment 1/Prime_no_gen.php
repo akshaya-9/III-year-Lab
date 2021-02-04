33b4dacecbfde93e4d59a765b6d82ece7075d5da
akshaya <55704423+akshaya-9@users.noreply.github.com>
2021-02-04 17:15:55 +0530

    Add files via upload

---
+++ CN&WT LAB/WT Experiments/Experiment 1/Prime_no_gen.php
@@ -0,0 +1,20 @@
+<?php
+$num = 1 ;
+while ($num < 51 ) {
+    $count=0;
+    for ( $i=1;$i<=$num;$i++) {
+        if (($num%$i)==0) {
+            $count++;
+        }
+    }
+    if ($count<3) {
+    echo $num." , ";
+    } 
+    $num=$num+1;
+}
+
+?>
+
+Output:
+
+2 , 3 , 5 , 7 , 11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47
