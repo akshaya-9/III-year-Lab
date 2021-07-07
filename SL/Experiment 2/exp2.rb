8fa36eec92c116c7e95a4c3597aeacc52897155c
akshaya <55704423+akshaya-9@users.noreply.github.com>
2021-07-07 07:25:19 +0530

    Add files via upload

---
+++ SL/Experiment 2/exp2.rb
@@ -0,0 +1,6 @@
+puts "Enter the value of radius:"
+radius = gets.chomp.to_f
+perimeter = 2 * 3.14 * radius
+area = 3.14 * radius * radius
+puts "The perimeter of a circle is #{perimeter}"
+puts "The Area of a circle is #{area}"
